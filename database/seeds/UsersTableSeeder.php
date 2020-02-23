5aa07f5e1271f3f9c9376550df4483e1bb184161
Bruno Moreira <bbjomoreira@gmail.com>
2020-02-23 00:21:30 -0300

    Creation UsersTableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name'  => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt(123456)
+        ]);
+
+        factory(App\User::class, 9)->create()->each(function ($user) {
+          $user->save();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('user');
+    }
+
+}
